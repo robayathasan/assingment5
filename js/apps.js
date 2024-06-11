4532f63865ffbf8f9d4294be2c14910084550a7e
robayathasan <robayathasan@gmail.com>
2024-06-12 02:13:50 +0600

    TriangleComplate

--- js/apps.js
+++ js/apps.js
@@ -1,1 +1,19 @@
-console.log("hi");
+
+document.getElementById('triangle-btn').addEventListener('click',function(){
+    const inputb = document.getElementById('input-b');
+    const inputh = document.getElementById('input-h');
+
+    const inputbString = inputb.value;
+    const inputhString = inputh.value;
+
+    const inputbNumber = parseInt(inputbString);
+    const inputhNumber = parseInt(inputhString);
+
+    const triangleResult = 0.5*inputbNumber*inputhNumber;
+
+    const calculatedarea = document.getElementById('resutlarea');
+    calculatedarea.innerText=triangleResult;
+
+    
+
+})
